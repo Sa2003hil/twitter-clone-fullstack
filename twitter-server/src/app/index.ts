57508449de083f0afca3f228d1141075fe9fd4a8
Sahil <Sahil2003@gmail.com>
2024-03-12 19:52:25 +0530

    Feat : connected graphQL with Prisma ORM

--- twitter-server/src/app/index.ts
+++ twitter-server/src/app/index.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import express from 'express';
 import bodyParser from 'body-parser';
+import { prismaClient } from '../clients/db';
 // to get the data from the server we query the data 
 // when we want to send data to the server we use mutation
 
@@ -9,6 +10,8 @@ export async function initServer(){
     const app = express();
 
     app.use(bodyParser.json());
+
+    
     const graphqlServer = new ApolloServer({
         // typeDefs is a string that contains the schema definition language (SDL) that defines the GraphQL schema.  
         typeDefs:`    
