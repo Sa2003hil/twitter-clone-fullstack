f20bd25ee6db87e66f5e072d4684688f1911010f
Sahil <Sahil2003@gmail.com>
2024-03-12 19:29:59 +0530

    Fix : Server Bug fixing done

---
+++ twitter-server/src/app/index.ts
@@ -0,0 +1,31 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import express from 'express';
+import bodyParser from 'body-parser';
+// to get the data from the server we query the data 
+// when we want to send data to the server we use mutation
+
+export async function initServer(){
+    const app = express();
+
+    app.use(bodyParser.json());
+    const graphqlServer = new ApolloServer({
+        // typeDefs is a string that contains the schema definition language (SDL) that defines the GraphQL schema.  
+        typeDefs:`    
+        type Query{
+            sayHello: String 
+        }
+
+        `,
+        resolvers:{
+            Query:{
+                sayHello:()=>`Hello from GraphQL Server!`
+            },
+        },
+      });
+
+    await graphqlServer.start();
+    app.use('/graphql',expressMiddleware(graphqlServer));
+
+    return app;
+}
--- twitter-server/src/index.ts
+++ twitter-server/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hi there from sahil server");
+import { initServer } from "./app";
+
+async function init(){
+    const app = await initServer();
+    app.listen(8000,()=> console.log(`Server is running on port 8000`));
+}
+
+init();
